6c85d4703307dc235fe14a7c0c033099d7c2d083
Jirachai C <antronic.inc@gmail.com>
2018-11-30 14:28:40 +0700

    Initial commit

--- C1-HelloWorld/HelloWorld.scala
+++ C1-HelloWorld/HelloWorld.scala
@@ -1,7 +1,5 @@
-object Hello{
-
-    def main(args: Array[String]):Unit = {
-        println("Hello World")
-    }
-
+object Hello {
+  def main(args: Array[String]): Unit = {
+    println("Hello World")
+  }
 }
